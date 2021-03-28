cb010bf1ad92779cbf4bce5f2fae75b91ebade6d
Maximiliano Arcieri <maxipiedras2001@gmail.com>
2021-03-27 22:09:34 -0300

    Delete de carpeta de ejercicios

--- Practica 2/Ejercicio9.py
+++
@@ -1,9 +0,0 @@
-texto = input('Ingrese una frase: ')
-
-ok = True
-for i in texto:
-    if ((texto.count(i) > 1) and (i != ' ')):
-        ok = False
-        break
-
-print('Es un heterograma') if ok else print('No es heterograma')
