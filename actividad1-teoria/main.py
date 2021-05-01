b99854c0bfb0ce1170a36db274be68b3571385f1
Maximiliano Arcieri <maxipiedras2001@gmail.com>
2021-05-01 00:44:53 -0300

    Actualizacion de archivos

--- actividad1-teoria/main.py
+++ actividad1-teoria/main.py
@@ -3,13 +3,13 @@ import analyze as an
 
 
 
-games_categories = ['ACTION','ADVENTURE','ARCADE','BOARD','CARD','CASINO','CASUAL','EDUCATIONAL','MUSIC','PUZZLE','RACING','ROLE PLAYING','SIMULATION','SPORTS','STRATEGY','TRIVIA','WORD']
+games_categories = ['ALL','ACTION','ADVENTURE','ARCADE','BOARD','CARD','CASINO','CASUAL','EDUCATIONAL','MUSIC','PUZZLE','RACING','ROLE PLAYING','SIMULATION','SPORTS','STRATEGY','TRIVIA','WORD']
 
 def create_layout():
 
     layout = [[sg.Text('Datos disponibles \npara analizar', font = '_ 20', relief = sg.RELIEF_RIDGE, justification = 'center', background_color = '#6E402A')],
             [sg.Text('', background_color = '#D89156')],
-            [sg.Button('Canciones de Spotify', size = (16,1), button_color = '#6E402A'), sg.Input('Artista', size = (12,1), key = 'artist')],
+            [sg.Button('Canciones de Spotify', size = (16,1), button_color = '#6E402A'), sg.Input('ALL', size = (12,1), key = 'artist')],
             [sg.Button('Juegos de Play Store', size = (16,1), button_color = '#6E402A'), sg.Combo(games_categories, size = (12,1), key = 'game_option')],
             [sg.Button('Salir', size = (6,1), button_color = '#6E402A')]]
     return sg.Window('Actividad 1 x Python Plus - TEORIA', layout, background_color = '#D89156', margins = (75,50), finalize = True)
@@ -26,7 +26,10 @@ def main():
             break
 
         elif event == 'Canciones de Spotify':
-            an.music_analysis(values.get('artist'))
+            if values.get('artist') != '':
+                an.music_analysis(values.get('artist'))
+            else:
+                sg.PopupQuick('Ingrese un valor válido', background_color = '#D89156', button_color = '#6E402A')
 
         elif event == 'Juegos de Play Store':
             if values.get('game_option') in games_categories:
